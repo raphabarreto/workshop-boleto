a2ea34e5b80aa525370b9f5bd958bd6410906348
Raphael Barreto <tnt.raphael@gmail.com>
2016-03-30 22:08:36 -0300

    JavaBeans do arquivo de retorno

---
+++ nova-cobranca-web/src/main/java/com/workshop/cobranca/util/retorno/bradesco/ArquivoRetorno.java
@@ -0,0 +1,10 @@
+package com.workshop.cobranca.util.retorno.bradesco;
+
+import java.util.List;
+
+public class ArquivoRetorno {
+
+	private Cabecalho cabecalho;
+	private List<TransacoesTitulo> titulos;
+	private Sumario sumario;
+}
